0bcae06f93ea24a409c136bceed61bca42c9e4c9
Nicholas-lh <350373023@gapps.yrdsb.ca>
2024-09-23 12:58:19 -0400

    Set position of penguin

--- MyWorld.java
+++ MyWorld.java
@@ -18,5 +18,10 @@ public class MyWorld extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
         
+        Penguin penguin = new Penguin();
+        addObject(penguin, 300, 200);
+        
+        
+        
     }
 }
